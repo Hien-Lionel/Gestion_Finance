8e1932650806ab1aa0851a72780777958e6b712f
Ashanti <hienlionel72@gmail.com>
2026-04-15 09:06:48 +0000

    Update build script

--- backend/build.sh
+++ backend/build.sh
@@ -13,3 +13,6 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
+
+# Create superuser automatically if it doesn't exist
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='ashanti').exists() or User.objects.create_superuser('ashanti', 'ashanti@example.com', '1234', role='admin')"
